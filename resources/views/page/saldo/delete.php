ae6098d71b78c17aeb875c1b0dfb2a98caa17d30
Keyza Fabiyan Taufik <dkeyza@gmail.com>
2023-01-16 11:51:51 +0700

    sudah di modifed

--- resources/views/page/saldo/delete.php
+++ resources/views/page/saldo/delete.php
@@ -2,5 +2,5 @@
     $konek = mysqli_connect('localhost','root','','simukas');  
     $id = $_GET["id"];  
     mysqli_query ($konek,"DELETE FROM saldo WHERE id= $id");
-    header("location:index.blade.php")
+    header("location:saldo.php")
 ?>
--- resources/views/page/saldo/edit.php
+++ resources/views/page/saldo/edit.php
@@ -1,7 +1,7 @@
 <?php
     $koneksi = mysqli_connect("localhost","root","","simukas");
     $id = $_GET["id"];
-    $kueri = mysqli_query($koneksi, "SELECT * FROM user WHERE id=$id");
+    $kueri = mysqli_query($koneksi, "SELECT * FROM saldo WHERE id=$id");
     $data = mysqli_fetch_assoc($kueri);
     ?>
 
@@ -11,7 +11,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form Edit</title>
+    <title>Form Ubah</title>
     <style>
         label,input{
             display: block;
@@ -21,21 +21,21 @@
 </head>
 <body>
 
-        <h1> edit user</h1>
+        <h1> edit saldo</h1>
     <form action="" method="post">
-        <label for="Nama_User">Nama user</label>
-    <input type="text" name="Nama_User" value="<?= $data['nama_user']?>" id="Nama_User">
-    <button type="submit" name="edit">Edit</button>
+        <label for="Jumlah_Saldo">jumlah saldo</label>
+    <input type="text" name="Nama_User" value="<?= $data['Jumlah_Saldo']?>" id="Jumlah_Saldo">
+    <button type="submit" name="Ubah">Ubah</button>
     <button type="submit" name="list">
-        <a href="index.blade.php " target="_blank" >Daftar User</a>
+        <a href="saldo.php " target="_blank" >Daftar Saldo</a>
     </button>
     </form>  
     <?php
-        if(isset($_POST["edit"])){
-            $nama_User  = $_POST['Nama_User'] ;
+        if(isset($_POST["Ubah"])){
+            $nama_User  = $_POST['Jumlah_Saldo'] ;
             
             mysqli_query($koneksi, "UPDATE User SET WHERE id = $id");
-            header("Location:index.blade.php");
+            header("Location:saldo.php");
         }
 ?>
 </body>
