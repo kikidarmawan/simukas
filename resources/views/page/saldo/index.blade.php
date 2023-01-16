35edfb64670ed7c5ddd07532090db896567211db
Keyza Fabiyan Taufik <dkeyza@gmail.com>
2023-01-16 15:57:05 +0700

    modifed

--- resources/views/page/saldo/index.blade.php
+++ resources/views/page/saldo/index.blade.php
@@ -4,9 +4,9 @@
 @endsection
 
 @push('style')
-    <link rel="stylesheet" href="/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-    <link rel="stylesheet" href="/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <link rel="stylesheet" href="/views/page/kegiatan/database.bootstrap4.min.css">
+    <link rel="stylesheet" href="/views/page/kegiatan/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="/views/page/kegiatan/buttons.bootstrap4.min.css">
 @endpush
 
 @section('content')
