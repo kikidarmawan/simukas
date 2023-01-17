2d2096905f811abcd90a4bcccc9216b6215fb502
Keyza Fabiyan Taufik <dkeyza@gmail.com>
2023-01-17 13:52:59 +0700

    modifed

---
+++ resources/views/page/kegiatan/index.blade.php
@@ -0,0 +1,115 @@
+@extends('layouts.panel')
+@section('title')
+    Data Saldo
+@endsection
+
+@push('style')
+    <link rel="stylesheet" href="/views/page/kegiatan/database.bootstrap4.min.css">
+    <link rel="stylesheet" href="/views/page/kegiatan/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="/views/page/kegiatan/buttons.bootstrap4.min.css">
+@endpush
+
+@section('content')
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Master Data</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="/dashboard">Dashboard</a></li>
+                        <li class="breadcrumb-item"><a href="#">Master Data</a></li>
+                        <li class="breadcrumb-item active"><a href="#">Saldo</a></li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+        <!-- Default box -->
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Data Saldo</h3>
+
+                <div class="card-tools">
+                    {{-- <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                        <i class="fas fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                        <i class="fas fa-times"></i>
+                    </button> --}}
+                    <a href="/master/saldo/create" class="btn btn-primary btn-sm"><i class="fas fa-plus"></i> Buat Saldo</a>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table" id="example1">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Nama Saldo</th>
+                                <th>Saldo</th>
+                                <th>Opsi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($data as $i)
+                                <tr>
+                                    <th>{{ $loop->iteration }}</th>
+                                    <td>{{ $i->nm_saldo }}</td>
+                                    <td>Rp.{{ number_format($i->jumlah, 2, '.', '.') }}</td>
+                                    <td>
+                                        <a href="" class="btn btn-sm btn-secondary">Lihat Riwayat</a>
+                                        <a href="" class="btn btn-sm btn-warning">Ubah</a>
+                                        <a href="" class="btn btn-sm btn-danger">Hapus</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
+            <!-- /.card-body -->
+            <div class="card-footer">
+
+            </div>
+            <!-- /.card-footer-->
+        </div>
+        <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
+@endsection
+
+@push('script')
+    <script src="/plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+    <script src="/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+    <script src="/plugins/jszip/jszip.min.js"></script>
+    <script src="/plugins/pdfmake/pdfmake.min.js"></script>
+    <script src="/plugins/pdfmake/vfs_fonts.js"></script>
+    <script src="/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+    <script src="/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+    <script src="/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+
+    <script>
+        $(function() {
+            $("#example1").DataTable({
+                "responsive": true,
+                "lengthChange": false,
+                "autoWidth": true,
+                "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+            }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+
+        })
+    </script>
+@endpush
