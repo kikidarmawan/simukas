68dc08e9650bec40f6d3c8beea4da74fcfd786fa
Keyza Fabiyan Taufik <dkeyza@gmail.com>
2023-01-20 09:20:54 +0700

    modifed

--- app/Models/Pengguna.php
+++ app/Models/Pengguna.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Kegiatan extends Model
+class Pengguna extends Model
 {
     use HasFactory;
     public $table = 'tb_pengguna';
